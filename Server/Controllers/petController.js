daa985cb58510497cbcdbf162f7782193ea7ed4b
StphnX <136083929+StphnX@users.noreply.github.com>
2023-09-18 15:04:36 +0200

    created a function to get all pets by animal_type

--- Server/Controllers/petController.js
+++ Server/Controllers/petController.js
@@ -1,12 +1,12 @@
 import asyncHandler from "express-async-handler";
-import Pet from "../schemas/petSchema.js";
+import PetModel from "../schemas/petSchema.js";
 
 //GET all pokemon
 const getAllPets = asyncHandler(async (req, res, next) => {
     const { query } = req.query;
 
     if (query) {
-        const results = await Pet.aggregate().search({
+        const results = await PetModel.aggregate().search({
             index: "pet",
             text: {
                 query,
@@ -16,67 +16,58 @@ const getAllPets = asyncHandler(async (req, res, next) => {
         });
         res.status(200).json(results);
     } else {
-        const results = await Pet.find({}, { name: 1, age: 1, gender: 1, animal_type: 1, breed: 1, health_status: 1, address: 1, zipcode: 1, _id: 0, description: 1 });
+        const results = await PetModel.find({}, { name: 1, age: 1, gender: 1, animal_type: 1, breed: 1, health_status: 1, address: 1, zipcode: 1, _id: 0, description: 1 });
 
         res.status(200).json(results);
     }
 });
 
 
-// GET single pokemon
 
-const getSinglePet = asyncHandler(async (req, res, next) => {
-    const { id } = req.params;
+const getSinglePet = asyncHandler(async (req, res) => {
+    try {
+        const id = req.params.id;
+        const pet = await PetModel.findById(id);
 
-    const searchProp = Number(id) ? "petId" : "name";
-    const searchValue = Number(id) || /id/i;
-    const filter = { [searchProp]: searchValue };
+        if (!pet) {
+            return res.status(404).json({ message: 'Pet not found' });
+        }
 
-    const result = (await Pet.findOne(filter)) ?? {};
-
-    if (!result) {
-        return res.status(400).json({
-            message: `Query for ${searchProp}: ${searchValue} did not return any results.`,
-        });
+        res.json(pet);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Internal server error' });
     }
-
-    res.status(200).json(result);
 });
 
-const getSinglePetInfo = asyncHandler(async (req, res, next) => {
-    const { id, prop } = req.params;
+const getAllPetsByProp = asyncHandler(async (req, res, next) => {
+    try {
+        const { animal_type } = req.query;
 
-    const searchProp = Number(id) ? "petId" : "name";
-    const searchValue = Number(id) || /id/i;
+        // Construct a filter object based on the provided query parameter
+        const filter = {};
 
-    const filter = { [searchProp]: searchValue };
-    const projection = { [prop]: 1 };
+        if (animal_type) {
+            filter.animal_type = animal_type;
+        }
 
-    const result = await Pet.findOne(filter, projection);
+        // Query the MongoDB collection using the filter
+        const pets = await PetModel.find(filter).exec();
 
-    if (!result) {
-        return res.status(400).json({
-            message: `Query for ${searchProp}: ${searchValue} did not return any results.`,
-        });
+        // Send the pets as a JSON response
+        res.json(pets);
+    } catch (error) {
+        // Handle any errors that occur during the query
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-
-    let retObj = result.toObject();
-
-    // if the prop we are looking for does not exist, this endpoint
-    // should not return anything... but mongoDB always returns an Object
-    // with a single property if the document exists (property _id)
-    if (Object.keys(retObj).length === 1) {
-        retObj = {};
-    }
-
-    res.status(200).json(retObj);
 });
+
 
 const postNewPet = asyncHandler(async (req, res) => {
     try {
         const newData = req.body;
 
-        const newPet = new Pet(newData);
+        const newPet = new PetModel(newData);
 
         await newPet.save()
         res.status(201).json({ message: 'New pet entry created successfully!', data: newPet });
@@ -93,4 +84,4 @@ const postNewPet = asyncHandler(async (req, res) => {
 
 
 
-export { getAllPets, getSinglePet, getSinglePetInfo, postNewPet };
+export { getAllPets, getSinglePet, postNewPet, getAllPetsByProp };
--- Server/Routes/petRouter.js
+++ Server/Routes/petRouter.js
@@ -2,15 +2,15 @@ import { Router } from "express";
 import {
     getAllPets,
     getSinglePet,
-    getSinglePetInfo,
+    getAllPetsByProp,
     postNewPet,
 } from "../Controllers/petController.js";
 
 const petRouter = Router();
 
 petRouter.route("/allPets").get(getAllPets);
-petRouter.route("/:id").get(getSinglePet);
-petRouter.route("/:id/:prop").get(getSinglePetInfo);
+petRouter.route("/Pet/:id").get(getSinglePet);
+petRouter.route("/:prop").get(getAllPetsByProp);
 petRouter.route("/Pet/create").post(postNewPet);
 // petRouter.route("/Pet/edit/:id").put(editNewPet);
 // petRouter.route("/Pet/delete/:id").delete(delNewPet);
