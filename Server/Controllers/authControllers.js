430673af6b46190bfdf4e2be9947df74d2177dc6
StphnX <136083929+StphnX@users.noreply.github.com>
2023-09-20 13:58:23 +0200

    added a Username to the userschema

--- Server/Controllers/authControllers.js
+++ Server/Controllers/authControllers.js
@@ -54,10 +54,10 @@ const login_get = (req, res) => {
 }
 
 const signup_post = async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password, Username } = req.body;
 
     try {
-        const user = await User.create({ email, password });
+        const user = await User.create({ email, password, Username });
         const token = createToken(user._id);
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
         res.status(201).json({ user: user._id });
--- Server/schemas/User.js
+++ Server/schemas/User.js
@@ -20,6 +20,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please enter a password'],
         minlength: [6, 'Minimum password length is 6 characters'],
+    },
+    Username: {
+        type: String,
+        required: [true, 'Please enter a Username'],
+        minlength: [4, 'Minimum length is 4 characters']
     }
 });
 
